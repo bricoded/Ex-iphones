528a80602022e9f11f674eeb03c27bd1df2d5632
bricoded <bacarybricoded@gmail.com>
2023-10-23 21:36:57 +0200

    finished the homepage UI

--- js/index.js
+++ js/index.js
@@ -3,58 +3,31 @@ const splide = new Splide('.splide', {
   drag: 'free',
   focus: 'center',
   perPage: 4,
+  perMove: 4,
   padding: '3rem',
   gap: '1rem',
-  autoScroll: {
-    speed: 1,
-  },
+  rewind: true,
+  rewindByDrag: true,
+  pagination: false,
+  autoplay: true,
+  speed: 400,
 });
 
 splide.mount();
 
-// const firstRequest = new XMLHttpRequest();
-// firstRequest.addEventListener('load', function() {
-//   console.log('IT WORKED!!!');
-//   console.log(this.responseText);
-// })
-// firstRequest.addEventListener('error', (error) => {
-//   console.log('ERROR!!!!', error)
-// })
-// firstRequest.open('GET', 'https://swapi.dev/api/planets/');
-// firstRequest.send();
-// console.log('Request sent');
+const splide2 = new Splide('#slider2', {
+  type: 'loop',
+  drag: 'free',
+  focus: 'center',
+  perPage: 4,
+  perMove: 4,
+  padding: '3rem',
+  gap: '1rem',
+  rewind: true,
+  rewindByDrag: true,
+  pagination: false,
+  autoplay: true,
+  speed: 400,
+})
 
-// fetch('https://swapi.dev/api/planets/')
-//   .then((reponse) => {
-//     if (!reponse.ok) {
-//       throw new Error('An error occured');
-//     }
-//     return reponse.json();
-//   })
-//   .then((data) => {
-//     const filmURL = data.results[0].films[0];
-//     return fetch(filmURL);
-//   })
-//   .then((reponse) => {
-//     if (!reponse.ok) {
-//       throw new Error('An error occured');
-//     }
-//     return reponse.json()
-//   })
-//   .then((data) => {
-//     console.log(data.title);
-//   })
-//   .catch((error) => {
-//     console.log('Error', error);
-//   })
-
-fetch('jdfifieieifeiferife')
-  .then((response) => {
-    if(!response.ok) {
-      throw new Error('An error occured');
-    }
-    return response.json()
-  })
-  .then((data) => {
-    console.log(data);
-  })
+splide2.mount();
